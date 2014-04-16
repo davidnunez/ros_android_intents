a3e90449b3d6b945e5928751467b35ad77ea93e5
David Nunez <david@davidnunez.com>
2014-04-16 15:58:37 -0400

    Initial Commit

---
+++ intent_handler/src/main/java/edu/mit/media/prg/ros_android_intents/intent_handler/IntentHandler.java
@@ -0,0 +1,15 @@
+package edu.mit.media.prg.ros_android_intents.intent_handler;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IntentHandler extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
